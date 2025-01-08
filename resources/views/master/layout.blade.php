3cd5b881790e80b466778da073f5663797cc5e2b
ainumaisarah <maisarahainu@gmail.com>
2025-01-09 01:30:50 +0800

    cantikkan profile

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -21,7 +21,8 @@
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/raty/2.7.1/jquery.raty.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
+
+</head>
   <body>
     <!--Top BE-->
 
--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,3 +1,4 @@
+<!-- filepath: /c:/xampp/htdocs/moonlit/resources/views/profile/show.blade.php -->
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -5,16 +6,18 @@
         </h2>
     </x-slot>
 
-    <div>
+    <div style="background-color: #f0f4f8;">
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
             @if (Laravel\Fortify\Features::canUpdateProfileInformation())
-                @livewire('profile.update-profile-information-form')
+                <div class="bg-white shadow sm:rounded-lg p-6">
+                    @livewire('profile.update-profile-information-form')
+                </div>
 
                 <x-section-border />
             @endif
 
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
-                <div class="mt-10 sm:mt-0">
+                <div class="bg-white shadow sm:rounded-lg p-6 mt-6">
                     @livewire('profile.update-password-form')
                 </div>
 
@@ -22,21 +25,21 @@
             @endif
 
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
-                <div class="mt-10 sm:mt-0">
+                <div class="bg-white shadow sm:rounded-lg p-6 mt-6">
                     @livewire('profile.two-factor-authentication-form')
                 </div>
 
                 <x-section-border />
             @endif
 
-            <div class="mt-10 sm:mt-0">
+            <div class="bg-white shadow sm:rounded-lg p-6 mt-6">
                 @livewire('profile.logout-other-browser-sessions-form')
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasAccountDeletionFeatures())
                 <x-section-border />
 
-                <div class="mt-10 sm:mt-0">
+                <div class="bg-white shadow sm:rounded-lg p-6 mt-6">
                     @livewire('profile.delete-user-form')
                 </div>
             @endif
