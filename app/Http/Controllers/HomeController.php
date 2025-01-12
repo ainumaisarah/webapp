e70f3fcd3985863b2153afd7f2aaaef2c6c31d27
ainumaisarah <maisarahainu@gmail.com>
2025-01-12 20:05:22 +0800

    try fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('mainpage'); // Return the view that represents your homepage
+        return view('mainpage');
+        // Return the view that represents your homepage
     }
 }
 
