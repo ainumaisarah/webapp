5f9d251cd256f90756fb9760dab9d9b0918d9c76
ainumaisarah <maisarahainu@gmail.com>
2025-01-13 16:57:59 +0800

    cantikkan sikit admin, reviews, rooms

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -11,6 +11,10 @@
     <div class="container">
       <div class="row no-gutters slider-text d-flex align-itemd-end justify-content-center">
         <div class="col-md-9 ftco-animate text-center d-flex align-items-end justify-content-center">
+            <div class="text">
+	            <p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.html">Admin</a></span> <span>Page</span></p>
+	            <h1 class="mb-4 bread">Admin</h1>
+            </div>
         </div>
       </div>
     </div>
--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -9,7 +9,7 @@
         <div class="row no-gutters slider-text d-flex align-items-end justify-content-center">
             <div class="col-md-9 ftco-animate text-center d-flex align-items-end justify-content-center">
                 <div class="text">
-                    <p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.html">Home</a></span> <span>About</span></p>
+                    <p class="breadcrumbs mb-2"><span class="mr-2"><a href="index.html">Home</a></span> <span>Reviews</span></p>
                     <h1 class="mb-4 bread">Reviews</h1>
                 </div>
             </div>
