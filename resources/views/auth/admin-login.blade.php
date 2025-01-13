da3a03e99cbd0f413a2f5e05ffaf8abc5b5d464a
ainumaisarah <maisarahainu@gmail.com>
2025-01-13 16:38:41 +0800

    betulkan logo kat login admin

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <img src="{{ asset('images/logo.png') }}" alt="Application Logo" class="h-10 w-auto">
         </x-slot>
 
         <div class="text-center mb-6">
