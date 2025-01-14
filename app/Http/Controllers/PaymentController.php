dd9bbb94375b0e83f6e10b2bf3e2506be43f08fa
Arii <arinasobhan75@gmail.com>
2025-01-14 17:03:52 +0800

    dah betulkan balik payment page

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -9,13 +9,13 @@ class PaymentController extends Controller
     public function index(Request $request)
     {
         $data = [
-            'booking_id' => $request->input('booking_id'),
-            'room_id' => $request->input('room_id'),
-            'room_type' => $request->input('room_type'),
-            'check_in_date' => $request->input('check_in_date'),
-            'check_out_date' => $request->input('check_out_date'),
-            'price' => $request->input('price'),
-            'guest_count' => $request->input('guest_count'),
+            'booking_id' => session('booking_id'),
+            'room_id' => session('room_id'),
+            'room_type' => session('room_type'),
+            'check_in_date' => session('check_in_date'),
+            'check_out_date' => session('check_out_date'),
+            'price' => session('price'),
+            'guest_count' => session('guest_count'),
         ];
         return view('payment')->with('data', $data);  // payment.blade.php
     }
--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -99,4 +99,3 @@
 }
 </style>
 @endsection
-
