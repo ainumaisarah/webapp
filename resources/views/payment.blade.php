d0ab77ed8a07dec5092832d4e9f11528f145fff0
Arii <arinasobhan75@gmail.com>
2025-01-14 17:09:06 +0800

    btulkan calculation totsla night price

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -46,9 +46,9 @@
             <div class="payment-summary">
                 <br>
                 <h3>Payment Summary</h3>
-                <p><strong>Subtotal:</strong> MYR {{ $data['price'] }}</p>
+                <p><strong>Subtotal:</strong> MYR {{ $data['price'] * \Carbon\Carbon::parse($data['check_in_date'])->diffInDays(\Carbon\Carbon::parse($data['check_out_date']))}}</p>
                 <p><strong>Total Tax:</strong> MYR {{ $data['price'] * 0.06 }}</p>
-                <p><strong>Total:</strong> MYR {{ $data['price'] + ($data['price'] * 0.06) }}</p>
+                <p><strong>Total:</strong> MYR {{ ($data['price'] * \Carbon\Carbon::parse($data['check_in_date'])->diffInDays(\Carbon\Carbon::parse($data['check_out_date'])) )+ ($data['price'] * 0.06) }}</p>
                 <br>
                 <h3>Cancellation Policy</h3>
                 <p><strong>Cancellation Fee:</strong> MYR 600.00</p>
